39a53964c96b4eaeef738f2e69941ca62ee6af8d
Viviane-Queiroz <vivianedsqueiroz@gmail.com>
2021-03-03 18:08:36 -0300

    chore: controller of added receipts

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
 var app = angular.module('receipeBox', []);
+
+app.controller('recipeBoxCtrl', function($scope){
+
+    $scope.saved = localStorage.getItem('_msecrist_recipebox');
+	$scope.recipeList = (localStorage.getItem('_msecrist_recipebox')!==null) ? JSON.parse($scope.saved) : [
+        {
+        title: "Pizza",
+        ingredients: "Cheese, Ham, Tomato"
+        }, {
+        title: "Ice-cream",
+        ingredients: "Milk, Chocolate, Sugar"
+        }, {
+        title: "Cheesecake",
+        ingredients: "Milk, cream, buscuits, sugar"
+        }, {
+        title: "Cake",
+        ingredients: "Milk, eggs, vanilla, chocolate"
+        }];
+});
+
+localStorage.setItem('_msecrist_recipebox', JSON.stringify($scope.recipeList));
